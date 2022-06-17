8f16220339cb6928f4b0424029eb714c961421e2
cubdesign <takeo@cubdesign.com>
2022-06-17 21:43:37 +0900

    drow text

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use image::{Rgba, RgbaImage};
+use imageproc::drawing::draw_text_mut;
+use rusttype::{Font, Scale};
+
 fn main() {
-    println!("Hello, world!");
+    let mut image = RgbaImage::new(512, 512);
+    let font = Vec::from(include_bytes!("../assets/fonts/Roboto-Regular.ttf") as &[u8]);
+    let font = Font::try_from_vec(font).unwrap();
+
+    let height = 300.0;
+    let scale = Scale {
+        x: height,
+        y: height,
+    };
+
+    let text = "a👾1";
+    draw_text_mut(
+        &mut image,
+        Rgba([255u8, 255u8, 255u8, 255u8]),
+        50,
+        0,
+        scale,
+        &font,
+        text,
+    );
+
+    image.save("test.jpg").unwrap();
 }
